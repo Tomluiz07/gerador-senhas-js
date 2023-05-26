1314554ea88b5679024647205eeb585329c13304
Wellingotn luiz <wellingtonluizwork@gmail.com>
2023-05-25 22:41:10 -0300

    Trabalhando no projeto

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let PassWordlength = 8
 const InputEl = document.querySelector('#password')
+const uppercaseCheck = document.querySelector('#uppercaseCheck')
+const Numbercheck = document.querySelector('#Number-check')
+const Simboloscheck = document.querySelector('#Simbolos-check')
 
 function CrietPassWord() {
-  const chars = "abcdefghijklmnopqrstuwxyvABCDEFGHKLMNOPQRSTUWXYZ123456789?!@&*"
+  const chars = "abcdefghijklmnopqrstuwxy"
+  const Uppercase = "ABCDEFGHKLMNOPQRSTUWXYZ"
+  const Numbers = "123456789"
+  const Symbus = "?!@&*"
+
+ if(uppercaseCheck.checked) {
+  chars =+ Uppercase
+ }
+ if(Numbercheck.checked) {
+  chars =+ Numbers
+ }
+ if( Simboloscheck.checked) {
+  chars =+ Symbus
+ }
 
   let PassWord = ""
 
@@ -19,11 +35,16 @@ function CrietPassWord() {
  function copy(){
     navigator.clipboard.writeText(InputEl.value)
   }
+
   const Copy01 = document.querySelector('#btn')
   Copy01.addEventListener('click', copy)
 
   const Copy02 = document.querySelector('#Copy-1') 
   Copy02.addEventListener('click', copy)
+
+  uppercaseCheck.addEventListener('click', CrietPassWord)
+  Numbercheck.addEventListener('click', CrietPassWord)
+  Simboloscheck.addEventListener('click', CrietPassWord)
 
   const PassWordElement = document.querySelector('#password-length')
    PassWordElement.addEventListener("input",  () =>{
